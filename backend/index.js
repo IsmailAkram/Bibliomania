cfe305fc851341f0cc1dec221885d7d286a0125c
Izzy A <54819038+IsmailAkram@users.noreply.github.com>
2023-12-01 10:58:48 -0500

    Created first HTTP route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,13 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  console.log(request);
+  return response
+    .status(200)
+    .send("Welcome to the MERN stack project, Bibliomania!");
+});
+
 app.listen(PORT, () => {
   console.log(`App is listening to port: ${PORT}`);
 });
