8e32f2281c75ef3fb646c43bf4c7be5eede13c81
Izzy A <54819038+IsmailAkram@users.noreply.github.com>
2023-12-01 11:16:44 -0500

    Added MongoDB and mongoose to Node.js

---
+++ README.md
@@ -0,0 +1,4 @@
+Note to self regarding:
+(node:17420) [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
+
+https://nodejs.org/api/punycode.html
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -10,6 +11,14 @@ app.get("/", (request, response) => {
     .send("Welcome to the MERN stack project, Bibliomania!");
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
-});
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
