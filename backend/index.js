a7fb2c31b0e6116fc7fc10db231a4cf38d2071c2
Izzy A <54819038+IsmailAkram@users.noreply.github.com>
2023-12-02 22:21:54 -0500

    Saved a new book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,44 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+// Middleware for parsing JSON request body
+app.use(express.json());
 
 app.get("/", (request, response) => {
   console.log(request);
   return response
     .status(200)
     .send("Welcome to the MERN stack project, Bibliomania!");
+});
+
+// Route for saving a new book
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(404).send({
+        message: "Please send all required fields: title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
