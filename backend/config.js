8387a6022be1f2afb13b802e7ea9c48cb4297080
Izzy A <54819038+IsmailAkram@users.noreply.github.com>
2025-03-05 14:49:31 -0500

    Update config.js
    connection string update

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  "mongodb+srv://<name>:<password>@bookstore-mern.ue4vyyh.mongodb.net/books-collection?retryWrites=true&w=majority";
+  // place your connection string here
